de6e7d08e9c9c7ce2bae4c180a481509bf4b3d10
Miguel Angel Beltran Rodriguez <41310419+MiguelBeltranRodriguez@users.noreply.github.com>
2018-08-07 16:44:04 -0500

    Add files via upload

--- script#15.R
+++ script#15.R
@@ -1,31 +1,46 @@
 #Punto-a
+f <- function(x) return(5*x-(exp(1)^x)-1)
 
+
+#Punto-b
+
+plot(f,from=-3,to=3)
+abline(h=0,col="blue");
+#raices: 0.5448804 y 2.396139
+
+
+#Punto-c
+g <- function(x) return((1+(exp(1)^x))/5)
+#intervalo de [0,2] y [2,3]
+
+
+#Punto-d
 
 puntofijo <-function(g, x0){
   maxIter=100
   tol=1e-8
   k = 1
-  # iteración hasta que abs(x1 - x0) <= tol o se alcance maxIteraciones
   repeat{
     x1 = g(x0)
-    print(x1)
+    
     dx = abs(x1-x0)
+    print(dx)
     x0 = x1
-    #Imprimir estado
     cat("x_", k, "= ", x1, "\n")
     k = k+1
     #until
     if(dx< tol|| k > maxIter) break;
   }
-  # Mensaje de salida
+  
   if( dx > tol ){
     cat("No hubo convergencia ")
-    #return(NULL)
   } else{
-    cat("x* es aproximadamente ", x1, " con error menor que ", tol)
+    cat("x* es aproximadamente ", x1, " con error menor que ", tol, "\n")
   }
+  cat("Error de truncamiento", abs(dx-tol))
 }
-g <- function(x) return(5*x-exp(1)^x-1)
 
+puntofijo(g,0)
+puntofijo(g,2.5)
+#Solo converge con la raiz de 0.5448804
 
-puntofijo(g,20)
